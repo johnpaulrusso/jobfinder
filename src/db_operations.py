19d8a308cf2d1b48130a0f9453c9433749dde92d
johnpaulrusso <jprusso93@gmail.com>
2024-10-28 16:17:36 -0500

    Only insert new jobs into the DB.
    Only insert new jobs into the DB.

--- src/db_operations.py
+++ src/db_operations.py
@@ -1,4 +1,5 @@
 from db_setup import dynamodb  # Import the initialized DynamoDB resource
+from botocore.exceptions import ClientError
 
 def insert_job_item(job_data):
     table = dynamodb.Table('jobs')
@@ -7,3 +8,11 @@ def insert_job_item(job_data):
         print("Job item inserted successfully:", response)
     except Exception as e:
         print(f"Error inserting item: {e}")
+
+def job_exists(job_data):
+    table = dynamodb.Table('jobs')  
+    try:
+        response = table.get_item(Key={"job_id": job_data["id"]})
+        return "Item" in response
+    except ClientError:
+        return False
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 import sys
 from fetch_page import fetch_page  
 from parse_page import parse_page
-from db_operations import insert_job_item
+from db_operations import insert_job_item, job_exists
 
 URL = "https://recruiting2.ultipro.com/SPA1004AURA/JobBoard/93330e50-7b3a-4ba8-94f2-6f32360aa4e1/?q=&o=postedDateDesc"  # Replace with your target URL
 
@@ -13,4 +13,8 @@ if not response:
 
 jobs = parse_page(response)
 for job in jobs:
-    insert_job_item(job)
+    if not job_exists(job):
+        insert_job_item(job)
+        ## TODO: Send email!
+    else:
+        print(f'Ignoring job post {job["id"]}, already tracked in DB.')
